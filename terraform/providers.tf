ae5b7ff37265ff739f047245353dfad23824c894
Brian Lalor <blalor@bravo5.org>
2022-08-20 16:37:51 -0400

    Terraform upgrade: 0.13.7

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,9 @@
 provider "aws" {
-    version = "~> 2.34"
     region  = var.aws_region
 }
 
 provider "aws" {
     alias = "us-east-1"
 
-    version = "~> 2.34"
     region  = "us-east-1"
-}
-
-provider "random" {
-    version = "~> 2.0"
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,9 +1,21 @@
 terraform {
-    required_version = ">= 0.12"
+    required_version = ">= 0.13"
 
     backend "s3" {
         # bucket = "…" ## partial
         # region = "…" ## partial
         key = "jekyll-site/terraform.tfstate"
+    }
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 2.34"
+        }
+
+        random = {
+            source = "hashicorp/random"
+            version = "~> 2.0"
+        }
     }
 }
